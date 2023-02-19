40301c211f14d21957341180b1133c4e10472cdd
Micode360 <abayemiracle@gmail.com>
2023-02-19 03:13:00 +0100

    back to one dot

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ app.use('/auth', authRoute)
 app.use('/os', privateRoute)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('./client/build'));
   app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname + '/client/build/index.html')));
 }
 
